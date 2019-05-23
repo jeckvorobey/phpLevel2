6b1c7543cfdee58585838d47f3d881ad7bba2e1b
jeckvorobey <jeckvorobey69@gmail.com>
2019-05-23 15:35:57 +0300

    Создал класс товара

---
+++ lesson1/good.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class Good
+{
+    private $author;
+    private $ttile;
+    private $prise;
+
+    public function __construct($author, $title, $price)
+    {
+        $this->setAuthor($author);
+        $this->setTitle($title);
+        $this->setPrice($price);
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function init()
+    {
+        echo '<div class="product">';
+        echo '<div class="author">Автор книги: '.$this->author.' </div>';
+        echo '<div class="title">Название: '.$this->title.' </div>';
+        echo '<div class="price">Стоимость: '.$this->price.' </div>';
+        echo '</div>';
+    }
+}
---
+++ lesson1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'good.class.php';
+
+$good = new Good('Мет Фицджеральд', 'Как сильно ты этого хочешь?', 200);
+$good->init();
