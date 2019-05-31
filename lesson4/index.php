abd03ef995c96bdea0cbc3b9e4c21414003d7a51
jeckvorobey <jeckvorobey69@gmail.com>
2019-05-31 12:56:46 +0300

    подготовил файлы для ДЗ

---
+++ lesson4/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Лист товаров</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <h1>Домашнее задание к 4 уроку.</h1>
+    </header>
+    <main>
+        <div class="product-catalog">
+            <div class="product">
+                <img src="https://via.placeholder.com/200x150.png" alt="picture">
+                <div class="content">
+                    <h4>Title</h4>
+                    <p class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dicta earum quos autem
+                        voluptas sapiente quisquam nihil adipisci sed perferendis.
+                    </p>
+                    <p class="price">200 руб.</p>
+                    <button class="buy" type="submit">В корзину</button>
+                </div>
+            </div>
+        </div>
+        <p class="more"><a href="#">Eщё...</a></p>
+    </main>
+    <footer class="footer">
+        <p> &copy; Все права защищены!</p>
+    </footer>
+</body>
+
+</html>
---
+++ lesson4/models/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'db_name' => 'goods',
+    'username' => 'root',
+    'password' => '123Qw!654',
+    'charset' => 'utf8',
+];
