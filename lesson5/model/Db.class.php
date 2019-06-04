6dafeadbee4d83bb4e83055ab72f8565f60e4748
jeckvorobey <jeckvorobey69@gmail.com>
2019-06-04 21:07:03 +0300

    промежутчные сохранения

---
+++ lesson5/model/Db.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class Db
+{
+    private $link;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        $config = require_once 'config.php';
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['db_name'].';charset='.$config['charset'];
+        $this->link = new PDO($dsn, $config['username'], $config['password']);
+
+        return $this;
+    }
+
+    public function exec($sql)
+    {
+        $sth = $this->link->prepare($sql);
+
+        return $sth->execute();
+    }
+
+    public function query($sql)
+    {
+        $sth = $this->link->prepare($sql);
+        $sth->execute();
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!$result) {
+            return [];
+        }
+
+        return $result;
+    }
+}
