ff24e847402609537809874ec8ff58d05c41a175
jeckvorobey <jeckvorobey69@gmail.com>
2019-06-07 09:33:48 +0300

    save

--- lesson6/lib/App.class.php
+++ lesson6/lib/App.class.php
@@ -20,7 +20,7 @@ class App
     protected static function web($url)
     {
         $url = explode("/", $url);
-        if (isset($url[0])) {
+        if (emty($url[0])) {
             $_GET['page'] = $url[0];//Часть имени класса контроллера
             if (isset($url[1])) {
                 if (is_numeric($url[1])) {
