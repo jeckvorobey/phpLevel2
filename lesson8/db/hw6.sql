71ba360d78e2382f35ad2537ede24e36d0eb4fb1
jeckvorobey <jeckvorobey69@gmail.com>
2019-07-07 20:47:59 +0300

    backUp DB

--- lesson8/db/hw6.sql
+++ lesson8/db/hw6.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Июн 28 2019 г., 17:13
+-- Время создания: Июл 07 2019 г., 20:46
 -- Версия сервера: 5.7.26-0ubuntu0.18.04.1
 -- Версия PHP: 7.2.19-0ubuntu0.18.04.1
 
@@ -28,12 +28,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `basket` (
   `id_basket` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `id_user` varchar(100) NOT NULL,
   `id_good` int(11) NOT NULL,
-  `price` double NOT NULL,
   `count` int(4) NOT NULL,
-  `is_in_order` tinyint(4) NOT NULL,
-  `id_order` int(11) NOT NULL
+  `date` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -61,6 +59,27 @@ INSERT INTO `categories` (`id_category`, `status`, `name`, `src`, `parent_id`) V
 (4, 1, 'Личный кабинет', 'user', 0),
 (5, 0, 'категория 5', '', 0),
 (6, 0, 'категория 6', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `delivery_id` int(11) NOT NULL,
+  `delivery_name` varchar(50) NOT NULL,
+  `delivery_price` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `delivery`
+--
+
+INSERT INTO `delivery` (`delivery_id`, `delivery_name`, `delivery_price`) VALUES
+(1, 'Самовывоз', 0),
+(2, 'Почтой РФ', 500),
+(3, 'Курьером', 300);
 
 -- --------------------------------------------------------
 
@@ -108,10 +127,38 @@ INSERT INTO `goods` (`id_good`, `name`, `price`, `id_category`, `status`) VALUES
 CREATE TABLE `order` (
   `id_order` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `amount` double NOT NULL,
-  `datetime_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `id_order_status` int(11) NOT NULL
+  `id_order_status` int(11) NOT NULL,
+  `datetime_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `order`
+--
+
+INSERT INTO `order` (`id_order`, `id_user`, `id_order_status`, `datetime_create`) VALUES
+(1, 1, 1, '2019-07-07 20:40:34');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_goods`
+--
+
+CREATE TABLE `order_goods` (
+  `id` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_good` int(11) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `order_goods`
+--
+
+INSERT INTO `order_goods` (`id`, `id_order`, `id_good`, `count`) VALUES
+(1, 1, 1, 4),
+(2, 1, 2, 5),
+(3, 1, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -123,6 +170,16 @@ CREATE TABLE `order_status` (
   `id_order_status` int(11) NOT NULL,
   `order_status_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `order_status`
+--
+
+INSERT INTO `order_status` (`id_order_status`, `order_status_name`) VALUES
+(1, 'Новый'),
+(2, 'В обработке'),
+(3, 'Отправлен'),
+(4, 'Исполнен');
 
 -- --------------------------------------------------------
 
@@ -173,8 +230,9 @@ CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `user_name` varchar(50) NOT NULL,
   `user_email` varchar(50) NOT NULL,
-  `user_password` varchar(100) NOT NULL,
+  `user_password` varchar(100) DEFAULT NULL,
   `phone` int(10) NOT NULL,
+  `user_adress` int(11) DEFAULT NULL,
   `user_date_reg` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -182,8 +240,9 @@ CREATE TABLE `user` (
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id_user`, `user_name`, `user_email`, `user_password`, `phone`, `user_date_reg`) VALUES
-(1, 'admin', 'test@test.ru', '41ebc4d177abc6a5f3dd56f8f557c31d67796e21e9bfe18b28907e0334f08e21', 77777777, '2019-06-28 14:29:40');
+INSERT INTO `user` (`id_user`, `user_name`, `user_email`, `user_password`, `phone`, `user_adress`, `user_date_reg`) VALUES
+(1, 'admin', 'test@test.ru', '41ebc4d177abc6a5f3dd56f8f557c31d67796e21e9bfe18b28907e0334f08e21', 77777777, NULL, '2019-06-28 14:29:40'),
+(7, 'test', '11@11', 'd2d54770ccf8ef90036a2b6579bae2fa6e063053de4fb1202f37fa8166db47ca', 11111, NULL, '2019-07-07 19:53:12');
 
 -- --------------------------------------------------------
 
@@ -202,7 +261,8 @@ CREATE TABLE `user_role` (
 --
 
 INSERT INTO `user_role` (`id_user_role`, `id_user`, `id_role`) VALUES
-(1, 1, 1);
+(1, 1, 1),
+(3, 7, 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -222,12 +282,30 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id_category`);
 
 --
+-- Индексы таблицы `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`delivery_id`);
+
+--
 -- Индексы таблицы `goods`
 --
 ALTER TABLE `goods`
   ADD PRIMARY KEY (`id_good`),
   ADD KEY `id_good` (`id_good`),
   ADD KEY `id_category` (`id_category`);
+
+--
+-- Индексы таблицы `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id_order`);
+
+--
+-- Индексы таблицы `order_goods`
+--
+ALTER TABLE `order_goods`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `order_status`
@@ -264,22 +342,37 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT для таблицы `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_basket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 --
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
   MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
+-- AUTO_INCREMENT для таблицы `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `delivery_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT для таблицы `goods`
 --
 ALTER TABLE `goods`
   MODIFY `id_good` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
+-- AUTO_INCREMENT для таблицы `order`
+--
+ALTER TABLE `order`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `order_goods`
+--
+ALTER TABLE `order_goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT для таблицы `order_status`
 --
 ALTER TABLE `order_status`
-  MODIFY `id_order_status` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_order_status` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT для таблицы `pages`
 --
@@ -289,12 +382,12 @@ ALTER TABLE `pages`
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id_user_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
